4f895430e54ce7ee89aeee39b6335b616f186738
JHenrique <henryque.pc.game@gmail.com>
2025-03-24 22:20:17 -0300

    aula 002 base dados SQLite

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
 
 Route::view('/','home');
+
+Route::get('/db',function(){
+
+    try {
+
+        DB::connection()->getPdo();
+        echo "Conexão com a base de dados com sucessos: " . DB::connection()->getDatabaseName();
+
+    } catch (\Exception $th) {
+        die("não foi possivel conectar ao banco de dados erro: " . $th->getMessage());
+    }
+});
