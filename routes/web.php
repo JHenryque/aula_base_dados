31ac2587e075eb9338fb5b2d128820597fe8f7ae
JHenrique <henryque.pc.game@gmail.com>
2025-03-26 21:27:25 -0300

    configuracao do mysql laravel e create um tabela migrations

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
@@ -40,4 +41,38 @@ Route::get('/mysql_test_two_databases',function(){
     } catch (\Exception $th) {
         die("não foi possivel conectar ao banco de dados erro: " . $th->getMessage());
     }
+});
+
+Route::get('/mysql_teste',function(){
+    try {
+        Config::set('database.connections.crud', [
+            'driver' => 'mysql',
+            'url' => '',
+            'host' => 'localhost',
+            'port' => 3306,
+            'database' => 'laravel_mysql_auth',
+            'username' => 'user_laravel_mysql_database',
+            'password' => 'vIJuFufof4X1VaxecoLu5eGA22SiF4',
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => env('DB_CHARSET', 'utf8mb4'),
+            'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                    PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') ]): [],
+            ]);
+
+        DB::connection('crud')->getPdo();
+        echo "sucessos: ";
+        echo '<hr>' . '<br>';
+    } catch (\Exception $th) {
+        die("não foi possivel conectar ao banco de dados erro: " . $th->getMessage());
+    }
+});
+
+Route::get('/mysql_user',function(){
+    DB::connection('mysql')->getPdo();
+    echo "ok";
 });
